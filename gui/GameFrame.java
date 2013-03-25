fa8a657ba2726e845d61646c549278ac141bdaf4
Knoweat <diogo24.jap@gmail.com>
2013-03-25 12:49:40 +0000

    added some gui files

--- gui/GameFrame.java
+++ gui/GameFrame.java
@@ -6,7 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JFormattedTextField;
+import javax.swing.JButton;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class GameFrame extends JFrame {
 
@@ -36,12 +38,27 @@ public class GameFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		JFormattedTextField frmtdtxtfldKdkd = new JFormattedTextField();
-		frmtdtxtfldKdkd.setText("kdkd");
-		contentPane.add(frmtdtxtfldKdkd, BorderLayout.CENTER);
+		setContentPane(contentPane);
+
+		final JPanel maze_panel = new MazePanel();
+		contentPane.add(maze_panel, BorderLayout.CENTER);
+
+		contentPane.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				maze_panel.requestFocusInWindow();
+			}
+		});
+
+		JPanel footer_panel = new JPanel();
+		contentPane.add(footer_panel, BorderLayout.SOUTH);
+
+		JButton btnNew = new JButton("New");
+		footer_panel.add(btnNew);
+
+		JButton btnClose = new JButton("Close");
+		footer_panel.add(btnClose);
 	}
 
 }
---
+++ gui/MazePanel.java
@@ -0,0 +1,12 @@
+package maze.gui;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class MazePanel extends JPanel {
+	public void paintComponent (Graphics g) {
+		super.paintComponent(g);
+		//g.draw
+	}
+}
