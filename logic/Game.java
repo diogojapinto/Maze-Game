a50d118e1d689e0e473ffba05af89d3796143820
Diogo Pinto <diogo24.jap@gmail.com>
2013-04-03 10:50:41 +0100

    added faulty srcs

--- logic/Game.java
+++ logic/Game.java
@@ -1,7 +1,21 @@
 package maze.logic;
 
+import maze.elems.Eagle;
+import maze.elems.Hero;
+import maze.elems.MazeExit;
+import maze.elems.Sword;
+
 public class Game {
 	private static Game instance = null;
+
+	public static Game getInstance() {
+		if (instance == null)
+			instance = new Game();
+		return instance;
+	}
 	
-	public static Game get
+	private Hero hero;
+	private Eagle eagle;
+	private Sword sword;
+	private MazeExit exit;
 }
